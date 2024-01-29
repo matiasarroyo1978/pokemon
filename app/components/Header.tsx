45acbfc46de931fad9fa0d27b92e95f386143281
Matias Arroyo <matiasarroyo684@gmail.com>
2024-01-29 11:06:36 -0300

    first commit

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useAuth } from "../context/FirebaseAuthContext";
 import {
-  HOME_ROUTE,
   LOGIN_ROUTE,
   PROFILE_ROUTE,
   REGISTER_ROUTE,
