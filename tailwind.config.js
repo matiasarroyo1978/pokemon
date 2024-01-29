2b0ac4b24e20bafa56b7bbcb4b1044837846d74d
Matias Arroyo <matiasarroyo684@gmail.com>
2024-01-29 17:25:34 -0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,13 @@
-/**
- * @format
- * @type {import('tailwindcss').Config}
- */
-
-// module.exports = {
-//   mode: "jit",
-//   darkMode: 'class', // or 'media' or 'class'
-//   purge: [
-//     "./app/**/*.{js,ts,jsx,tsx}",
-//     "./pages/**/*.{js,ts,jsx,tsx}",
-//     "./components/**/*.{js,ts,jsx,tsx}",
-//   ],
-//   content: [
-//     "./app/**/*.{js,ts,jsx,tsx}",
-//     "./pages/**/*.{js,ts,jsx,tsx}",
-//     "./components/**/*.{js,ts,jsx,tsx}",
-
-//     // Or if using `src` directory:
-//     "./src/**/*.{js,ts,jsx,tsx}",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// };
 module.exports = {
   mode: "jit",
-  darkMode: 'class', // or 'media' or 'class'
-  purge: {
-    content: [
-      "./app/**/*.{js,ts,jsx,tsx}",
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",
-      // Or if using `src` directory:
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-  },
+  darkMode: "class", // or 'media' or 'class'
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
